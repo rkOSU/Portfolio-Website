f0ccc47bfee1c6e2934bf519462c6bf9c557036b
Raj Kondaveeti <46794263+rkOSU@users.noreply.github.com>
2021-01-26 14:57:39 -0500

    fixed typos

--- src/components/About.js
+++ src/components/About.js
@@ -13,10 +13,10 @@ function About() {
                     <p>Hey there! I'm Raj and I love spending time in nature
                         and going backpacking whenever I can scrape up the time. A few years back, I chose to specialize in artificial intelligence
                         and have found the classes in this track to be very interesting. For that reason, I wish
-                        to venture into software development which incoroprates topics such as machine learning
+                        to venture into software development which incorporates topics such as machine learning
                         and neural networks. </p>
                     <p>Apart from that, I also find agricultrue very interesting and often think about the integration of
-                        it with technology. In fact, the picutre on the left is from an agriculture service trip I took to Uruguay
+                        it with technology. In fact, the picture on the left is from an agriculture service trip I took to Uruguay
                         over a winter break! Feel free to contact me if you would like to get in touch.
          
                     </p>
